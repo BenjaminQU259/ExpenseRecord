e4b21e6f53bc73b065ff80afac1221a130066511
Minghao QU <MQu2@slb.com>
2022-10-19 15:10:01 +0800

    feat: add service for program.cs

--- ExpenseRecord/Program.cs
+++ ExpenseRecord/Program.cs
@@ -1,16 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ExpenseRecord;
+using ExpenseRecord.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+  options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.AddScoped<IExpenseItemService, ExpenseItemService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
