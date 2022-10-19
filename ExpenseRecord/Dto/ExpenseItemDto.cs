d3510c7420a60fa3fb17f0b1c68b7827c2be77b5
Minghao QU <MQu2@slb.com>
2022-10-19 14:06:30 +0800

    feat: add Dto for expense items

---
+++ ExpenseRecord/Dto/ExpenseItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseRecord.Dto
+{
+  public class ExpenseItemDto
+  {
+    [Column("exp_desc")]
+    public string? Description { get; set; }
+    [Column("exp_type")]
+    public string? Type { get; set; }
+    [Column("exp_amount")]
+    public int Amount { get; set; }
+    [Column("exp_date")]
+    public DateTime Date { get; set; }
+  }
+}
